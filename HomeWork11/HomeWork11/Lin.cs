55f3bc5c19c3af61eb32fa5c9cfa13bb4301442b
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-16 00:04:23 +0300

    Добавьте файлы проекта.

---
+++ HomeWork11/HomeWork11/Lin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal class Lin //linear equation 
+    {
+        double k;
+        double b;
+        public Lin(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public double Root() 
+            { return (-b / k); }
+    }
+}
---
+++ HomeWork11/HomeWork11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Разработать структуру для решения линейного уравнения 0=kx+b. \n" +
+                "Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.\n" +
+                "Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры.\n" +
+                "Осуществить использование экземпляра в программе.");
+
+            Console.Write("k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Lin lin =new Lin(k, b);
+            Console.WriteLine("x={0}",lin.Root());
+            Console.ReadKey();
+        }
+    }
+}
